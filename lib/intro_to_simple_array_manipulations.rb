2c0915a648f7357777cacd2a32273a25f6e3acf6
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-24 19:01:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array,element)
-   element.insert 
+def using_insert(array)
+   array.insert(element)
 end
 
 def using_uniq(array)
