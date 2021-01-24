e95b765bf26f31e120cda4170c0a28f55413097f
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-24 19:16:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array1, array2, array3, array4)
-   array.insert(array5)
+def using_insert(array, elemnt)
+   array.insert(4,element)
 end
 
 def using_uniq(array)
